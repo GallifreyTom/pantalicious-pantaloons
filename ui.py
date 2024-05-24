ce747f7deff92b88486d0a8bffe5c4b76d0ca605
GallifreyTom <18beaumontbrownt@clevedonschool.org.uk>
2024-05-24 14:49:21 +0100

    Added comments and checkboxes for different colours of pantaloons

--- ui.py
+++ ui.py
@@ -11,9 +11,31 @@ window.resizable(False, False)
 
 #Window Elements
 
+#Title
 titleName = tk.Label(window, text="Pantalicious Pantaloons", font=("Papyrus", 24, "bold"))
 titleName.pack()
+
+#Subtitle
 subTitle = tk.Label(window, text="Order your pantaloons today!", font=("Papyrus", 16, "italic"))
 subTitle.pack()
+
+#checkboxes
+
+#Blue
+blue_pantaloon = tk.IntVar()
+blue_pantaloon_button = tk.Checkbutton(window, text="Blue", variable=blue_pantaloon, onvalue=1, offvalue=0)
+blue_pantaloon_button.pack()
+
+#Red
+red_pantaloon = tk.IntVar()
+red_pantaloon_button = tk.Checkbutton(window, text="Red", variable=red_pantaloon, onvalue=1, offvalue=0)
+red_pantaloon_button.pack()
+
+#Cream
+cream_pantaloon = tk.IntVar()
+cream_pantaloon_button = tk.Checkbutton(window, text="Cream", variable=cream_pantaloon, onvalue=1, offvalue=0)
+cream_pantaloon_button.pack()
+
+
 
 window.mainloop()
